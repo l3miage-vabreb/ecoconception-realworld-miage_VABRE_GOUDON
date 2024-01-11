65759ff0a8593d46be84eeebf53015a68125c8df
Vabre ben <naruben74@hotmail.fr>
2024-01-11 15:10:33 +0100

    WIP

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -1,20 +1,27 @@
+// animation.component.ts
 import { Component } from '@angular/core';
-import { trigger, style, animate, transition } from '@angular/animations';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-animation',
   templateUrl: './animation.component.html',
   styleUrls: ['./animation.component.css'],
   animations: [
-    trigger('rotate', [
-      style({ transform: 'rotate(0deg)' }),
-      animate('2000ms', style({ transform: 'rotate(360deg)' }))
+    trigger('rotateImage', [
+      state('rotated', style({
+        transform: 'rotate(360deg)'
+      })),
+      state('notRotated', style({
+        transform: 'rotate(0deg)'
+      })),
+      transition('rotated <=> notRotated', animate('500ms ease-in-out'))
     ])
   ]
 })
-
 export class AnimationComponent {
+  rotateState: string = 'notRotated';
 
-
+  toggleRotation() {
+    this.rotateState = (this.rotateState === 'notRotated') ? 'rotated' : 'notRotated';
+  }
 }
